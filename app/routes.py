fde4bfe61bd5a39e16be05a4b5b2ba737bc78f8f
NPozza119 <noe.pozza@gmail.com>
2021-12-15 15:09:46 +0100

    add setupbd

--- app/routes.py
+++ app/routes.py
@@ -21,25 +21,7 @@ from app.models.activity import Activity
 
 
 from app.TEMPORAIREaddSomeData import setUpDB
-#setUpDB()
-
-#creation of default user (admin)
-user = User(username = "123", firstname = "Jean-Pierre", lastname = "Polochon", date=date(1975,7,22), email="JPP@gmail.com", photo = "app/static/image/JP.jfif")
-user.set_password("admin")
-db.session.add(user)
-
-persoJPGroup = Group(Name= "Your calendar") #Personnal group of admin user
-db.session.add(persoJPGroup)
-
-JPLink = BelongTo(idUser=User.query.filter_by(username="123").first().id,idGroup=Group.query.filter_by(Name="Your calendar").first().id)
-db.session.add(JPLink)
-
-task1 = Activity(name = "Étudier", description = "Min: 7h", dateDebut = datetime.fromisoformat("2021-12-20T14:00:00"), idGroup = Group.query.filter_by(Name="Your calendar").first().id)
-task2 = Activity(name = "Dormir", description = "Pour quoi faire", dateDebut = datetime.fromisoformat("2021-12-03T15:00:00"), idGroup = Group.query.filter_by(Name="Your calendar").first().id)
-db.session.add(task1)
-db.session.add(task2)
-
-db.session.commit()
+setUpDB()
 
 
 #+---------------+
