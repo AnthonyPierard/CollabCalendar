9b69ae7f418d812484d05fea2263fad16424d698
maxptrnk <maxime.ptrnk@gmail.com>
2021-12-30 22:35:56 +0100

    ajouts mineurs : navbar sur page login

--- app/static/JS/loadNewEvent.js
+++ app/static/JS/loadNewEvent.js
@@ -1,114 +1,25 @@
 
-// TODO : fix le url, sauvegarder les données
-
-
-// function submitContactForm(){
-//         var reg = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-//         var name = $('#taskname').val();
-//         var date = $('#datename').val();
-       
-//         if(name.trim() == '' ){
-//             $('.statusMsg').html('<span style="color:red;">Please enter your name.</p>');
-//             // alert('Please enter your name.');
-//             $('#taskname').focus();
-//             return false;
-//         }else if(date.trim() == '' ){
-//             $('.statusMsg').html('<span style="color:red;">Please enter a date.</p>');
-//             $('#inputMessage').focus();
-//             return false;
-//         }else{
-
-            
-            
-//             $.ajax({
-//                 type:'POST',
-//                 url:'/new_activity',
-//                 data:{nameoftask : $('#taskname').val(),
-//                       dateoftask : $('#datename').val()},
-//                 // //useless
-//                 // beforeSend: function () {
-//                 //     $('.submitBtn').attr("disabled","disabled");
-//                 //     $('.modal-body').css('opacity', '.5');
-//                 // },
-   
-//                 success:function(msg){
-//                     // // test
-//                     alert(
-//                         $('#datename').val()
-//                     )
-//             $('.statusMsg').html('<span style="color:red;">.....Need to add new in database....</p>');
-//             $('#taskname').val('');
-//             $('#datename').val('');
-//             $('.submitBtn').removeAttr("disabled");
-//             $('.modal-body').css('opacity', '');
-
-//                     if(msg == 'ok'){
-//                         $('#taskname').val('');
-//                         $('#datename').val('');
-//                         $('.statusMsg').html('<span style="color:green;">task added</p>');
-//                     }else{
-//                         $('.statusMsg').html('<span style="color:red;">task not added</span>');
-//                     }
-//                     $('.submitBtn').removeAttr("disabled");
-//                     $('.modal-body').css('opacity', '');
-//                 }
-//             });
-//         }
-//     }
-
-
-// function submitContactForm(){
-
-//         $('form').on('submit', function(event) {
-    
-//             $.ajax({
-//                 data : {
-//                     name : $('#nameInput').val(),
-//                     email : $('#emailInput').val()
-//                 },
-//                 type : 'POST',
-//                 url : '/new_activity'
-//             })
-//             .done(function(data) {
-    
-//                 if (data.error) {
-//                     $('#errorAlert').text(data.error).show();
-//                     $('#successAlert').hide();
-//                 }
-//                 else {
-//                     $('#successAlert').text(data.name).show();
-//                     $('#errorAlert').hide();
-//                 }
-    
-//             });	
-    
-//             event.preventDefault();
-    
-//         });
-    
-//     };
 
 
 $(document).ready(function() {
-
 	$('form').on('submit', function(event) {
 
+		//send informations on task from fronted to backend
 		$.ajax({
 			data : {
+				//takes data on task from the html form
 				name : $('#nameInput').val(),
 				description : $('#descriptionInput').val(),
                 dateBegin : $('#dateBeginInput').val(),
                 interval : $('#intervalInput').val(),
 				idGroup : $("#groupSelect").val(),
-
 			},
 			type : 'POST',
 			url : '/new_activity'
 		})
 		.done(function(data) {
-
 			if (data.error) {
-				// $('#errorAlert').text(data.error).show();
+				alert("Error: problem occured while adding new task ")	
 			}
 			else {
             	calendar.render();
@@ -125,7 +36,6 @@ $(document).ready(function() {
 function modifyActivity() {
 
 	$('#formModifyTask').on('submit', function(event) {
-
 		$.ajax({
 			data : {
 				taskid : $('#idhidden').val(),
@@ -133,7 +43,6 @@ function modifyActivity() {
 				description : $('#hiddenNewDescription').val(),
                 dateBegin : $('#hiddenNewDate').val(),
                 interval : $('#hiddenNewInterval').val(),
-
 			},
 			type : 'POST',
 			url : '/modify_activity'
@@ -145,12 +54,10 @@ function modifyActivity() {
 			$('#newDateInput').empty()
 			$('#newIntervalInput').empty()
 
-
-			if (data.error) {
-				
+			if (data.error) {		
+				alert("Error: problem occured while modifying task ")	
 			}
 			else {
-				
                 calendar.render();
             	sideBarLoader()
 			}
@@ -162,7 +69,6 @@ function modifyActivity() {
 
 
 function removeActivity() {
-
 	$.post(
 		"/remove_activity",
 		{
@@ -170,20 +76,15 @@ function removeActivity() {
 		}
 	).done( data => {
 		if (data = "succes"){
-  
-			// $("#deleteTaskMessage").append(`<p style="color: green;"> Task was successfully deleted </p>`)
-			
 			$('#ShowTaskModal').modal('hide');
 			location.reload();
-			// calendar.render();
-            // sideBarLoader()
-	  
-		  }else{alert("Error: problem occured while deleting task ")}
+		  }else{
+			  alert("Error: problem occured while deleting task ")
+			}
 
 	}).fail( _ => {
 		alert("Error: server isn't reachable")
 	})
-
 }
 
 
